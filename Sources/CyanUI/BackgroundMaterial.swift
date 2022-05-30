b2b7792fa4912ea9707e343085b400705130ab16
ktiays <cuansky@vip.qq.com>
2022-05-31 00:55:36 +0800

    add `BackgroundMaterial` for macOS

---
+++ Sources/CyanUI/BackgroundMaterial.swift
@@ -0,0 +1,32 @@
+//
+//  Created by ktiays on 2022/5/31.
+//  Copyright (c) 2022 ktiays. All rights reserved.
+//
+
+#if canImport(AppKit)
+
+import SwiftUI
+
+@available(macOS 11.0, *)
+public struct BackgroundMaterial: View {
+    
+    let blendColor: Color?
+    
+    public init(blendColor: Color? = nil) {
+        self.blendColor = blendColor
+    }
+    
+    public var body: some View {
+        VisualEffectView(
+            state: .followsWindowActiveState,
+            material: .hudWindow,
+            blendingMode: .behindWindow
+        )
+        .overlay(
+            blendColor ?? Color.windowBackground
+                .opacity(0.4)
+        )
+    }
+}
+
+#endif
