eb2d9fc5e17d30739f07eb8cf21488d7fdc3df82
ktiays <cuansky@vip.qq.com>
2022-10-09 22:32:36 +0800

    add `PillBanner`

---
+++ Sources/CyanUI/Banner/PillContainerViewController.swift
@@ -0,0 +1,43 @@
+//
+//  Created by ktiays on 2022/10/9.
+//  Copyright (c) 2022 ktiays. All rights reserved.
+//
+
+#if canImport(UIKit)
+
+import UIKit
+import SwiftUI
+
+@available(iOS 15.0, *)
+class PillContainerHostingView: UIView {
+    
+    private let hostingView: _UIHostingView<PillContainerView>
+    
+    init(rootView: PillContainerView) {
+        hostingView = _UIHostingView(rootView: rootView)
+        hostingView.backgroundColor = .clear
+        
+        super.init(frame: .zero)
+
+        addSubview(hostingView)
+        hostingView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            hostingView.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+            hostingView.trailingAnchor.constraint(equalTo: self.trailingAnchor),
+            hostingView.topAnchor.constraint(equalTo: self.topAnchor),
+            hostingView.bottomAnchor.constraint(equalTo: self.bottomAnchor),
+        ])
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func hitTest(_ point: CGPoint, with event: UIEvent?) -> UIView? {
+        let view = super.hitTest(point, with: event)
+        return (view === self || view === hostingView) ? nil : view
+    }
+    
+}
+
+#endif
