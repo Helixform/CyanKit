2c420ddf7e479e2c34cb9070c000711a12f7671c
ktiays <cuansky@vip.qq.com>
2023-01-07 21:20:33 +0800

    Add more types support for `Defaults`

--- Sources/CyanUtils/Defaults.swift
+++ Sources/CyanUtils/Defaults.swift
@@ -8,36 +8,65 @@ import Foundation
 // MARK: Property Wrapper for UserDefaults
 
 public protocol ConstructibleFromDefaults {
-    
     static func from(_ defaults: UserDefaults, with key: String) -> Self?
-    
 }
 
 extension Int: ConstructibleFromDefaults {
-    
     public static func from(_ defaults: UserDefaults, with key: String) -> Self? {
-        return defaults.integer(forKey: key)
+        defaults.integer(forKey: key)
     }
-    
+}
+
+extension Float: ConstructibleFromDefaults {
+    public static func from(_ defaults: UserDefaults, with key: String) -> Float? {
+        defaults.float(forKey: key)
+    }
+}
+
+extension Double: ConstructibleFromDefaults {
+    public static func from(_ defaults: UserDefaults, with key: String) -> Double? {
+        defaults.double(forKey: key)
+    }
 }
 
 extension String: ConstructibleFromDefaults {
-    
     public static func from(_ defaults: UserDefaults, with key: String) -> Self? {
-        return defaults.string(forKey: key)
+        defaults.string(forKey: key)
     }
-    
 }
 
 extension Bool: ConstructibleFromDefaults {
-    
     public static func from(_ defaults: UserDefaults, with key: String) -> Self? {
-        return defaults.bool(forKey: key)
+        defaults.bool(forKey: key)
     }
-    
 }
 
-@propertyWrapper public struct Defaults<T> where T: ConstructibleFromDefaults {
+extension Array: ConstructibleFromDefaults where Element: ConstructibleFromDefaults {
+    public static func from(_ defaults: UserDefaults, with key: String) -> Array<Element>? {
+        defaults.array(forKey: key) as? Self
+    }
+}
+
+extension URL: ConstructibleFromDefaults {
+    public static func from(_ defaults: UserDefaults, with key: String) -> URL? {
+        defaults.url(forKey: key)
+    }
+}
+
+extension Dictionary: ConstructibleFromDefaults where Key == String, Value: ConstructibleFromDefaults {
+    public static func from(_ defaults: UserDefaults, with key: String) -> Dictionary<String, Value>? {
+        defaults.dictionary(forKey: key) as? Self
+    }
+}
+
+extension Data: ConstructibleFromDefaults {
+    public static func from(_ defaults: UserDefaults, with key: String) -> Data? {
+        defaults.data(forKey: key)
+    }
+}
+
+@propertyWrapper
+public struct Defaults<T> where T: ConstructibleFromDefaults {
     
     public let key: String
     public let defaultValue: T
@@ -49,7 +78,7 @@ extension Bool: ConstructibleFromDefaults {
     
     public var wrappedValue: T {
         get {
-            return .from(.standard, with: key) ?? defaultValue
+            .from(.standard, with: key) ?? defaultValue
         }
         
         nonmutating set {
@@ -57,4 +86,30 @@ extension Bool: ConstructibleFromDefaults {
         }
     }
     
+}
+
+public protocol DefaultsCompatible {
+    static func defaultValue() -> Self
+}
+
+extension Int: DefaultsCompatible {
+    public static func defaultValue() -> Self { 0 }
+}
+
+extension Float: DefaultsCompatible {
+    public static func defaultValue() -> Self { 0 }
+}
+
+extension Double: DefaultsCompatible {
+    public static func defaultValue() -> Self { 0 }
+}
+
+extension Bool: DefaultsCompatible {
+    public static func defaultValue() -> Self { false }
+}
+
+extension Defaults where T: DefaultsCompatible {
+    public init(key: String) {
+        self.init(key: key, defaultValue: T.defaultValue())
+    }
 }
